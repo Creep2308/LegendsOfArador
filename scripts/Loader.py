eed351416dfbbbc1d8795dd60cff870155d85c61
Marcel <marcelkaemper862@gmail.com>
2018-02-14 16:26:41 +0100

    Update

---
+++ scripts/Loader.py
@@ -0,0 +1,21 @@
+def Load():
+	print("***************************\n")
+	print("Wähle deine Klasse: \n")
+	print("***************************\n")
+	
+	#Auswahl
+	print("[1]Krieger\n")
+	print("[2]Magier\n")
+	print("[3]Waldläufer\n")
+	print("***************************\n")
+		
+	aktion = input("> ")
+	
+	if "1" in aktion:
+	    pass
+	elif "2" in aktion:
+	    pass
+	elif "3" in aktion:
+	    pass
+	
+	# 3 Spieldateien für jede Klasse 
