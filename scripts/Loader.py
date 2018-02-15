f905b91707c738fad2edec27b6b4281c0ddeb649
Marcel Kämper <marcelkaemper862@gmail.com>
2018-02-15 17:12:59 +0100

    Update Loader.py

--- scripts/Loader.py
+++ scripts/Loader.py
@@ -9,8 +9,7 @@ def Load():
 	print("[2]Magier\n"); time.sleep(0.1)
 	print("[3]Waldläufer\n"); time.sleep(0.1)
 	print("***************************\n"); time.sleep(0.1)
-	
-<<<<<<< HEAD
+
 	while(1):
 		aktion = input("> ")
 		if aktion == "1" or aktion == "2" or aktion == "3":
@@ -18,17 +17,5 @@ def Load():
 				break
 		else:
 			print("Something went wrong")
-
+			
 	# 3 Spieldateien für jede Klasse 
-=======
-	if "1" in aktion:
-	    return(1)
-	elif "2" in aktion:
-	    return(2)
-	elif "3" in aktion:
-		return(3)
-	else:
-		print("Wrong input")
-	
-	# 3 Spieldateien für jede Klasse 
->>>>>>> 5511373d94b5d9a4840e55e5c1a813a23ea983ea
