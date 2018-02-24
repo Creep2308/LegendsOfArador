001b2ff03e0af6e6e2010db7278fb051f0d9dc0c
Marcel <marcelkaemper862@gmail.com>
2018-02-24 17:43:02 +0100

    Update

--- scripts/Loader.py
+++ scripts/Loader.py
@@ -7,29 +7,27 @@ def load():
     EscapeSeq.Escape()
     #Remove the Placeholder if it exists
     if Path("data/characters/GitPlaceholder").is_file():
-        deletePlaceholder()
+        os.remove("data/characters/GitPlaceholder")
     
     print("***************************\n"); sleep(0.25)
     print("Choose your Character: \n"); sleep(0.25)
-    print("***************************\n"); sleep(0.25)
+    print("***************************"); sleep(0.25)
 
     dir = os.listdir("data/characters/")
 
     #Print the content of the directory
     for i in range(len(dir)):
         print("["+str(i+1)+"]"+str(dir[i]))
-    print(dir)
     while 1:
         aktion = input("> ")
         try:
-            print(ReadCharData(dir[int(aktion)-1]))
+            print(ReadCharData(dir[int(aktion)]) if aktion == "0" else ReadCharData(dir[int(aktion)-1]))
         except Exception as e:
             print("Something went wrong: "+str(e))
-            sleep(0.5)
+            print("Reloading!")
+            sleep(0.75)
             load()
 
-def deletePlaceholder():
-    os.remove("data/characters/GitPlaceholder")
 
 def ReadCharData(para_name):
     with open("data/characters/"+para_name, "r") as r:
